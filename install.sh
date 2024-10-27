e82db80f7f5a4f8dc3e2dd4336d0255e5117b751
Rinshad-parancheri <rinshadp014@gmail.com>
2024-10-27 07:34:49 +0530

    migrate into stow
    change the file structure completely to stow and
    migrated from bash to zsh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Colors for output
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+print_status() {
+  echo -e "${GREEN}[*] $1${NC}"
+}
+
+print_error() {
+  echo -e "${RED}[!] $1${NC}"
+}
+
+set -e
+
+mkdir -p $HOME/.local/bin
+
+sudo pacman -S - <packages.txt
+
+for dir in */; do
+  dir=${dir%/}
+  echo "Stowing $dir..."
+  stow "$dir"
+done
+
+echo "All configurations have been stowed successfully."
+print_status "Reloading systemd..."
+sudo systemctl daemon-reload
+
+print_status "Enabling and starting the battery notification timer..."
+systemctl --user enable battery-notification.timer
+systemctl --user start battery-notification.timer
+
+source .zshrc
+
+print_status "Setup complete!"
